2ca2c64c9cd119ea2c2340338718b69868f3e3b8
Doug Hoyte <doug@hcsw.org>
2014-12-31 03:01:37 +0000

    Import of FRACTAL/Inline-Filters-Ragel-0.101.tar.gz from CPAN.
    gitpan-cpan-distribution: Inline-Filters-Ragel
    gitpan-cpan-version:      0.101
    gitpan-cpan-path:         FRACTAL/Inline-Filters-Ragel-0.101.tar.gz
    gitpan-cpan-author:       FRACTAL
    gitpan-cpan-maturity:     released

--- lib/Inline/Filters/Ragel.pm
+++ lib/Inline/Filters/Ragel.pm
@@ -1,6 +1,6 @@
 package Inline::Filters::Ragel;
 
-our $VERSION = '0.100';
+our $VERSION = '0.101';
 
 use strict;
 
@@ -9,11 +9,11 @@ use File::Temp;
 
 require Exporter;
 use base 'Exporter';
-our @EXPORT = qw(ragel);
+our @EXPORT = qw(ragel); ## filters protocol doesn't require export: this is just for back-compat
 
 
 
-sub ragel {
+sub filter {
   my $args;
 
   if (@_ == 1) {
@@ -64,6 +64,10 @@ sub ragel {
   };
 }
 
+
+*ragel = \&filter; ## back-compat
+
+
 1;
 
 
@@ -80,17 +84,24 @@ Inline::Filters::Ragel - Run ragel when compiling your Inline modules
 
     use Inline::Filters::Ragel;
 
-    use Inline C => <<'END', filters => [ ragel ];
+    use Inline C => <<'END', FILTERS => [ [ Ragel => '-C' ] ];
       // ragel/C code goes here
     END
 
 =head1 DESCRIPTION
 
-This module exports one "factory" function, C<ragel>. This function returns an anonymous function that accepts a string input, pre-processes it with the C<ragel> binary, and returns the output. The C<ragel> "factory" function can optionally take a string or multiple strings which will be passed along to the ragel binary. You will need to do this if you are compiling a language other than the default (C/C++), or if you wish to change the ragel state-machine compilation type.
+This module is designed to be used with the L<Inline> modules that provide the C<FILTERS> feature. It provides a "factory" function, C<filters>. This function returns an anonymous function that accepts a string input, pre-processes it with the C<ragel> binary, and returns the output. The C<filters> "factory" function can optionally take a string or multiple strings which will be passed along to the ragel binary. You will need to do this if you are compiling a language other than the default (C/C++), or if you wish to change the ragel state-machine compilation type.
 
-Note that you will need to download and install L<Ragel|http://www.colm.net/open-source/ragel/> before this module will work. It is my hope that modules will not require ragel at distribution time since we now have L<Inline::Module> (but I haven't tested that yet).
+B<NOTE>: You will need to download and install L<Ragel|http://www.colm.net/open-source/ragel/> before this module will work. Additionally, for the C<FILTERS> syntax in the synopsis you will need L<Inline::C> version 0.72 or higher.
 
-This module itself does not actually depend on any L<Inline> stuff so it may be useful as a stand-alone C<ragel> invoker module.
+This module itself does not actually depend on any L<Inline> stuff so it may be useful as a stand-alone ragel invoker module. Note that an alias for C<filters> called C<ragel> is exported for backwards compatibility.
+
+=head1 DEBUGGING
+
+If you set the C<INLINE_FILTERS_RAGEL_DEBUG> environment variable, the filter will produce the following output and will not clean-up the directory mentioned:
+
+    Inline::Filters::Ragel args: $VAR1 = '-C -G2';
+      --> See input/output files in /tmp/3QJBxyCy9m
 
 =head1 FULL EXAMPLE
 
@@ -98,22 +109,22 @@ As an example, here is the definition of an C<is_valid_utf8> function which uses
 
     use Inline::Filters::Ragel;
 
-    use Inline C => <<'END', FILTERS => [ ragel('-G2') ];
+    use Inline C => <<'END', FILTERS => [ [ Ragel => '-C -G2' ] ];
       %%{
         machine utf8_checker;
 
         ## Adapted from: http://www.w3.org/International/questions/qa-forms-utf-8
 
-        utf8_codepoint = (0x09 | 0x0A | 0x0D | 0x20..0x7E)            | # ASCII
-                         (0xC2..0xDF 0x80..0xBF)                      | # non-overlong 2-byte
-                         (0xE0 0xA0..0xBF 0x80..0xBF)                 | # excluding overlongs
-                         ((0xE1..0xEC | 0xEE | 0xEF) (0x80..0xBF){2}) | # straight 3-byte
-                         (0xED 0x80..0x9F 0x80..0xBF)                 | # excluding surrogates
-                         (0xF0 0x90..0xBF (0x80..0xBF){2})            | # planes 1-3
-                         (0xF1..0xF3 (0x80..0xBF){3})                 | # planes 4-15
-                         (0xF4 0x80..0x8F (0x80..0xBF){2});             # plane 16
+        codepoint = (0x09 | 0x0A | 0x0D | 0x20..0x7E)            | # ASCII
+                    (0xC2..0xDF 0x80..0xBF)                      | # non-overlong 2-byte
+                    (0xE0 0xA0..0xBF 0x80..0xBF)                 | # excluding overlongs
+                    ((0xE1..0xEC | 0xEE | 0xEF) (0x80..0xBF){2}) | # straight 3-byte
+                    (0xED 0x80..0x9F 0x80..0xBF)                 | # excluding surrogates
+                    (0xF0 0x90..0xBF (0x80..0xBF){2})            | # planes 1-3
+                    (0xF1..0xF3 (0x80..0xBF){3})                 | # planes 4-15
+                    (0xF4 0x80..0x8F (0x80..0xBF){2});             # plane 16
 
-        main := utf8_codepoint*;
+        main := codepoint*;
 
         write data;
       }%%
@@ -138,6 +149,7 @@ As an example, here is the definition of an C<is_valid_utf8> function which uses
         return 1;
       }
     END
+
 
 =head1 SEE ALSO
 
